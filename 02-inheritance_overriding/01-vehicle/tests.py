80ad04cc32378931c8eec1af3ec444558aee378c
U0139490 <serhaterdogan2011@hotmail.com>
2023-01-10 19:06:50 +0100

    unittests

--- 02-inheritance_overriding/01-vehicle/tests.py
+++ 02-inheritance_overriding/01-vehicle/tests.py
@@ -2,52 +2,51 @@ import unittest
 from student import *
 
 class TestVehicleExercise(unittest.TestCase):
+    def setUp(self):
+        self.vehicle1 = Vehicle("Toyota", "Corolla", 2020)
+        self.car1 = Car("Toyota", "Corolla", 2020, 4)
+        self.truck1 = Truck("Toyota", "Tacoma", 2020, "large")
+
+
     def test_create_vehicle(self):
-        vehicle = Vehicle("Toyota", "Corolla", 2020)
-        self.assertEqual(vehicle.make, "Toyota")
-        self.assertEqual(vehicle.model, "Corolla")
-        self.assertEqual(vehicle.year, 2020)
-        self.assertEqual(vehicle.speed, 0)
+        self.assertEqual(self.vehicle1.make, "Toyota")
+        self.assertEqual(self.vehicle1.model, "Corolla")
+        self.assertEqual(self.vehicle1.year, 2020)
+        self.assertEqual(self.vehicle1.speed, 0)
 
     def test_vehicle_accelerate(self):
-        vehicle = Vehicle("Toyota", "Corolla", 2020)
-        self.assertEqual(vehicle.speed, 0)
+        self.assertEqual(self.vehicle1.speed, 0)
         vehicle.accelerate()
-        self.assertEqual(vehicle.speed, 10)
+        self.assertEqual(self.vehicle1.speed, 10)
         vehicle.accelerate()
-        self.assertEqual(vehicle.speed, 20)
+        self.assertEqual(self.vehicle1.speed, 20)
 
     def test_vehicle_brake(self):
-        vehicle = Vehicle("Toyota", "Corolla", 2020)
-        vehicle.speed = 20
-        vehicle.brake()
-        self.assertEqual(vehicle.speed, 13)
-        vehicle.brake()
-        self.assertEqual(vehicle.speed, 6)
+        self.vehicle1.speed = 20
+        self.vehicle1.brake()
+        self.assertEqual(self.vehicle1.speed, 13)
+        self.vehicle1.brake()
+        self.assertEqual(self.vehicle1.speed, 6)
 
     def test_vehicle_honk(self):
-        vehicle = Vehicle("Toyota", "Corolla", 2020)
-        self.assertEqual(vehicle.honk(), "HONK!")
+        self.assertEqual(self.vehicle1.honk(), "HONK!")
 
     def test_create_car(self):
-        car = Car("Toyota", "Corolla", 2020, 4)
-        self.assertEqual(car.make, "Toyota")
-        self.assertEqual(car.model, "Corolla")
-        self.assertEqual(car.year, 2020)
-        self.assertEqual(car.speed, 0)
-        self.assertEqual(car.num_doors, 4)
+        self.assertEqual(self.car1.make, "Toyota")
+        self.assertEqual(self.car1.model, "Corolla")
+        self.assertEqual(self.car1.year, 2020)
+        self.assertEqual(self.car1.speed, 0)
+        self.assertEqual(self.car1.num_doors, 4)
 
     def test_car_honk(self):
-        car = Car("Toyota", "Corolla", 2020, 4)
-        self.assertEqual(car.honk(), "Beep beep!")
+        self.assertEqual(self.car1.honk(), "Beep beep!")
 
     def test_create_truck(self):
-        truck = Truck("Toyota", "Tacoma", 2020, "large")
-        self.assertEqual(truck.make, "Toyota")
-        self.assertEqual(truck.model, "Tacoma")
-        self.assertEqual(truck.year, 2020)
-        self.assertEqual(truck.speed, 0)
-        self.assertEqual(truck.bed_size, "large")
+        self.assertEqual(self.truck1.make, "Toyota")
+        self.assertEqual(self.truck1.model, "Tacoma")
+        self.assertEqual(self.truck1.year, 2020)
+        self.assertEqual(self.truck1.speed, 0)
+        self.assertEqual(self.truck1.bed_size, "large")
 
 
 if __name__ == "__main__":
