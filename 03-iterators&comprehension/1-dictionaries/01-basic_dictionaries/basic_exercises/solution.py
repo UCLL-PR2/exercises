f284ff8801c07bc01182ba8636bb869b7165c803
U0139490 <serhaterdogan2011@hotmail.com>
2023-02-01 10:21:41 +0100

    solution

--- 03-iterators&comprehension/1-dictionaries/01-basic_dictionaries/basic_exercises/solution.py
+++ 03-iterators&comprehension/1-dictionaries/01-basic_dictionaries/basic_exercises/solution.py
@@ -1,32 +1,32 @@
-def create_dict():
+def create_dict() -> dict:
     return {"Name": "John", "Age": 30, "City": "New York"}
 
-def add_country(dictionary):
+def add_country(dictionary: dict) -> dict:
     dictionary["Country"] = "USA"
     return dictionary
 
-def get_age(dictionary):
+def get_age(dictionary: dict) -> int:
     return dictionary["Age"]
 
-def update_age(dictionary, new_age):
+def update_age(dictionary: dict, new_age: int) -> dict:
     dictionary["Age"] = new_age
     return dictionary
 
-def remove_city(dictionary):
+def remove_city(dictionary: dict) -> dict:
     dictionary.pop("City", None)
     return dictionary
 
-def check_country(dictionary):
+def check_country(dictionary: dict) -> bool:
     return "Country" in dictionary
 
-def get_keys(dictionary):
+def get_keys(dictionary: dict) -> list:
     return list(dictionary.keys())
 
-def get_values(dictionary):
+def get_values(dictionary: dict) -> list:
     return list(dictionary.values())
 
-def create_dict2():
+def create_dict2() -> dict:
     return {"Name": "Jane", "Age": 25, "City": "San Francisco"}
 
-def merge_dicts(dict1, dict2):
+def merge_dicts(dict1: dict, dict2: dict) -> dict:
     return {**dict1, **dict2}
