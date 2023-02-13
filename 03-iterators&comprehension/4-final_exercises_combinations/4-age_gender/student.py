0abceffe166cb88f8a2f9bbde56530b645e99531
U0139490 <serhaterdogan2011@hotmail.com>
2023-02-13 19:10:50 +0100

    slide

--- 03-iterators&comprehension/4-final_exercises_combinations/4-age_gender/student.py
+++ 03-iterators&comprehension/4-final_exercises_combinations/4-age_gender/student.py
@@ -3,13 +3,3 @@ def read_data(filename):
         lines = f.read().splitlines()
         data = [line.split(",") for line in lines]
         return data
-
-dictionary = {
-    1: "A",
-    2: "B",
-    3: "C"
-}
-#Getting the keys
-print(list(dictionary.keys()))
-#Getting the values
-print(list(dictionary.values()))
