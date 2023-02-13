1b72bf6cdb84cb9ce06901dd885825f7c7270c62
U0139490 <serhaterdogan2011@hotmail.com>
2023-02-13 20:38:56 +0100

    solution

--- 03-iterators&comprehension/4-final_exercises_combinations/3-most_common_words/solution.py
+++ 03-iterators&comprehension/4-final_exercises_combinations/3-most_common_words/solution.py
@@ -1,14 +1,17 @@
 def create_word_dict(file_name):
     with open(file_name, 'r') as f:
-        word_list = f.read().split()
+        word_list = f.read()
         word_list = word_list.lower()
+        word_list = word_list.split()
+        # word_list = f.read().lower().split()
+
         word_list = [word.strip('.,?!-') for word in word_list]
         word_list = list(filter(lambda x: x.isalpha(), word_list))
         word_dict = {word: word_list.count(word) for word in word_list}
     return word_dict
 
-# word_dict = create_word_dict('example.txt')
-# print(word_dict)
+word_dict = create_word_dict('example.txt')
+print(word_dict)
 
 def get_top_10_words(word_dict):
     sorted_words = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
