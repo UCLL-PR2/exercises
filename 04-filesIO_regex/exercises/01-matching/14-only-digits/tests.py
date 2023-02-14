e20c757ed1cc9027990c0042344593a253f56f4f
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-14 18:25:31 +0100

    04-filesIO_regex/exercises/01-matching/14-only-digits

--- 04-filesIO_regex/exercises/01-matching/14-only-digits/tests.py
+++ 04-filesIO_regex/exercises/01-matching/14-only-digits/tests.py
@@ -1,25 +1,43 @@
-from contextlib import contextmanager
-from scripting.testing import test
-from scripting.quick import regex_test
-from scripting.assertions import assert_truthy, assert_falsey
+import pytest
+import student
+import solution
 
 
-with regex_test('only_digits') as (match, no_match):
-    match('')
-    match('0')
-    match('1')
-    match('2')
-    match('3')
-    match('4')
-    match('5')
-    match('6')
-    match('7')
-    match('8')
-    match('9')
-    match('1235020284')
+@pytest.mark.parametrize("string", [
+    '',
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '00',
+    '11',
+    '22',
+    '33',
+    '44',
+    '55',
+    '66',
+    '77',
+    '88',
+    '99',
+    '48865',
+    'a',
+    '15 98',
+    '4879 ',
+])
+def test_function(string):
+    function_name = 'only_digits'
+    assert hasattr(student, function_name), f"Missing function {function_name}"
 
-    no_match('a')
-    no_match('d')
-    no_match('125f')
-    no_match('78a3')
-    no_match('q2100')
+    solution_function = getattr(solution, function_name)
+    student_function = getattr(student, function_name)
+
+    actual = bool(student_function(string))
+    expected = bool(solution_function(string))
+
+    assert expected == actual, f"Wrong result for {string}, expected {expected}, received {actual}"
