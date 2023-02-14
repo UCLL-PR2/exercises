f8db543a81d6051e58bff45010af09d3903571fc
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-14 23:22:42 +0100

    04-regex-io/exercises/03-substituting/03-correct-dates

--- 04-regex-io/exercises/03-substituting/03-correct-dates/tests.py
+++ 04-regex-io/exercises/03-substituting/03-correct-dates/tests.py
@@ -1,12 +1,22 @@
-from contextlib import contextmanager
-from scripting.testing import test
-from scripting.quick import reference_based_test
-from scripting.reference import active_reference_implementation_from_id, reference_file
+import pytest
+import student
+import solution
 
 
-with reference_file('solution.py'):
-    with reference_based_test('correct_dates') as testcase:
-        testcase('1/2/3')
-        testcase('12/11/2019')
-        testcase('12/11/2019 6/7/1899')
-        testcase('12/11/2019 fjklfjl 6/7/1899')
+@pytest.mark.parametrize("string", [
+    '1/2/3',
+    '12/11/2019',
+    '12/11/2019 6/7/1899',
+    '12/11/2019 fjklfjl 6/7/1899',
+])
+def test_function(string):
+    function_name = 'correct_dates'
+    assert hasattr(student, function_name), f"Missing function {function_name}"
+
+    solution_function = getattr(solution, function_name)
+    student_function = getattr(student, function_name)
+
+    actual = student_function(string)
+    expected = solution_function(string)
+
+    assert expected == actual, f"Wrong result for {string}, expected {expected}, received {actual}"
