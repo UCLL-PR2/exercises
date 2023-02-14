5cc2faa943276cf81f570038f61facf38d7c4b85
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-14 18:25:32 +0100

    04-filesIO_regex/exercises/01-matching/16-is-dna

--- 04-filesIO_regex/exercises/01-matching/16-is-dna/tests.py
+++ 04-filesIO_regex/exercises/01-matching/16-is-dna/tests.py
@@ -1,23 +1,22 @@
-from contextlib import contextmanager
-from scripting.testing import test
-from scripting.quick import regex_test
-from scripting.assertions import assert_truthy, assert_falsey
+import pytest
+import student
+import solution
 
 
-with regex_test('is_dna') as (match, no_match):
-    match('')
-    match('G')
-    match('A')
-    match('T')
-    match('C')
-    match('CGAT')
-    match('GCCTATAGTAGACG')
-    match('CCCCGGGGAAAATTTT')
+@pytest.mark.parametrize("string", [
+    '',
+    *'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    'GATTACA',
+    'GARTACA',
+])
+def test_function(string):
+    function_name = 'is_dna'
+    assert hasattr(student, function_name), f"Missing function {function_name}"
 
-    no_match('a')
-    no_match('g')
-    no_match('c')
-    no_match('t')
-    no_match('q')
-    no_match('Q')
-    no_match('PGATCCCC')
+    solution_function = getattr(solution, function_name)
+    student_function = getattr(student, function_name)
+
+    actual = bool(student_function(string))
+    expected = bool(solution_function(string))
+
+    assert expected == actual, f"Wrong result for {string}, expected {expected}, received {actual}"
