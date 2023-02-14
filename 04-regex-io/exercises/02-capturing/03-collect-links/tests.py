96354331db1bd6183f76c587dfcc2869f6682a91
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-14 22:54:11 +0100

    04-regex-io/exercises/02-capturing/03-collect-links

--- 04-regex-io/exercises/02-capturing/03-collect-links/tests.py
+++ 04-regex-io/exercises/02-capturing/03-collect-links/tests.py
@@ -1,12 +1,12 @@
-from contextlib import contextmanager
-from scripting.testing import test
-from scripting.quick import reference_based_test
-from scripting.reference import active_reference_implementation_from_id, reference_file
 
-
-with reference_file('solution.py'):
-    with reference_based_test('collect_links') as testcase:
         testcase('''
+
+        ''')
+
+
+
+@pytest.mark.parametrize("string", [
+    """
         <a href="a">fdf</a>
         fjklfjls
         <a href="b">qff</a>
@@ -14,5 +14,22 @@ with reference_file('solution.py'):
         <a href="abc">dfdg</a>
         fdjflkjdlf
         <a href="fiop">fdghh</a>
-        ''')
+        fjdlfjlk
+        <a href=""></a>
+    """,
+    """
+        <a href="url1">text1</a>
+        <a href="url2">text2</a><a href="url3">text3</a>
+    """
+])
+def test_function(string):
+    function_name = 'collect_links'
+    assert hasattr(student, function_name), f"Missing function {function_name}"
 
+    solution_function = getattr(solution, function_name)
+    student_function = getattr(student, function_name)
+
+    actual = student_function(string)
+    expected = solution_function(string)
+
+    assert expected == actual, f"Wrong result for {string}, expected {expected}, received {actual}"
