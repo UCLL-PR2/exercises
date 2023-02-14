dcaffd59cb918712433d9793c6e910786f3bcfcb
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-14 18:25:32 +0100

    04-filesIO_regex/exercises/01-matching/18-contains-a

--- 04-filesIO_regex/exercises/01-matching/18-contains-a/tests.py
+++ 04-filesIO_regex/exercises/01-matching/18-contains-a/tests.py
@@ -1,15 +1,24 @@
-from contextlib import contextmanager
-from scripting.testing import test
-from scripting.quick import regex_test
-from scripting.assertions import assert_truthy, assert_falsey
+import pytest
+import student
+import solution
 
 
-with regex_test('contains_a') as (match, no_match):
-    match('a')
-    match('ab')
-    match('aaaa')
-    match('xax')
-    match('xxaa')
+@pytest.mark.parametrize("string", [
+    '',
+    'a',
+    'aaaa',
+    'abc',
+    '      a       ',
+    'xxxxxa',
+])
+def test_function(string):
+    function_name = 'contains_a'
+    assert hasattr(student, function_name), f"Missing function {function_name}"
 
-    no_match('')
-    no_match('b')
+    solution_function = getattr(solution, function_name)
+    student_function = getattr(student, function_name)
+
+    actual = bool(student_function(string))
+    expected = bool(solution_function(string))
+
+    assert expected == actual, f"Wrong result for {string}, expected {expected}, received {actual}"
