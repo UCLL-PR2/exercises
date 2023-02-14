42c9b376ae5b835e7eba0c0f919c07fee5aedeeb
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-14 22:57:33 +0100

    04-regex-io/exercises/02-capturing/04-scrape-email-addresses

--- 04-regex-io/exercises/02-capturing/04-scrape-email-addresses/tests.py
+++ 04-regex-io/exercises/02-capturing/04-scrape-email-addresses/tests.py
@@ -1,27 +1,23 @@
-from contextlib import contextmanager
-from scripting.testing import test
-from scripting.quick import reference_based_test
-from scripting.reference import active_reference_implementation_from_id, reference_file
-
-
-with reference_file('solution.py'):
-    with reference_based_test('scrape_email_addresses') as testcase:
-        testcase('''
-        a@c.com
-        ''')
-
-        testcase('''
-        111@AFF.com
-        ''')
-
-        testcase('''
-        111@AFF.com
-        ''')
-
-        testcase('''
+@pytest.mark.parametrize("string", [
+        'a@c.com',
+        '111@AFF.com',
+        '*a@b*',
+        """
         a@c.com fsjdf jfslk fkls fjl df
         jalfkj b@d.be fjdlkf jfkljdlkf
         qpoiopc fdfqpof ifppopo fkpqo
         qfjlkl xyz@ppp.fr jkfljqlkj
-        ''')
+        opfpqsjkl<...@...>
+        """,
+])
+def test_function(string):
+    function_name = 'scrape_email_addresses'
+    assert hasattr(student, function_name), f"Missing function {function_name}"
 
+    solution_function = getattr(solution, function_name)
+    student_function = getattr(student, function_name)
+
+    actual = student_function(string)
+    expected = solution_function(string)
+
+    assert expected == actual, f"Wrong result for {string}, expected {expected}, received {actual}"
