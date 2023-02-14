521236a7140ebbe2daabfdd3e17487505eae37fb
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-14 17:26:49 +0100

    04-filesIO_regex/exercises/01-matching/04-one-or-more-a

--- 04-filesIO_regex/exercises/01-matching/04-one-or-more-a/tests.py
+++ 04-filesIO_regex/exercises/01-matching/04-one-or-more-a/tests.py
@@ -11,4 +11,13 @@ import solution
     "aaaaab",
 ])
 def test_function(string):
-    assert bool(student.one_or_more_a(string)) == bool(solution.one_or_more_a(string))
+    function_name = 'one_or_more_a'
+    assert hasattr(student, function_name), f"Missing function {function_name}"
+
+    solution_function = getattr(solution, function_name)
+    student_function = getattr(student, function_name)
+
+    actual = bool(student_function(string))
+    expected = bool(solution_function(string))
+
+    assert expected == actual, f"Wrong result for {string}, expected {expected}, received {actual}"
