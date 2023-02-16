37776a09fbd31f857e9fdc5b88ed4b0f311b4c7a
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-16 22:07:50 +0100

    02-oo/properties/time

---
+++ 02-oo/properties/time/assignment.md
@@ -0,0 +1,11 @@
+# Assignment
+
+We want to keep track of time of days, i.e. a time between 00:00:00 and 23:59:59.
+Write a class `Time` that represents such a time of day.
+
+* It must keep track of hours, minutes and seconds.
+* The value of `hours` must be between 0 and 23.
+* The value of `minutes` and seconds must be between 0 and 59.
+* The constructor takes three parameters `hours`, `minutes` and `seconds` and uses them to initialize the object's attributes.
+* The values of `hours`, `minutes` and `seconds` must be settable.
+* Rely on properties to guard `hours`, `minutes` and `seconds` against invalid values.
---
+++ 02-oo/properties/time/tests.py
@@ -0,0 +1,70 @@
+import pytest
+from student import *
+
+
+@pytest.fixture
+def time():
+    return Time(0, 0, 0)
+
+
+@pytest.mark.parametrize("hours, minutes, seconds", [
+    (h, m, s)
+    for h in [0, 4, 23]
+    for m in [0, 12, 59]
+    for s in [0, 43, 59]
+])
+def test_constructor_valid_values(hours, minutes, seconds):
+    time = Time(hours, minutes, seconds)
+
+    assert time.hours == hours
+    assert time.minutes == minutes
+    assert time.seconds == seconds
+
+
+@pytest.mark.parametrize("hours, minutes, seconds", [
+    (-1, 0, 0),
+    (24, 0, 0),
+    (0, -1, 0),
+    (0, 60, 0),
+    (0, 0, -1),
+    (0, 0, 60)
+])
+def test_constructor_invalid_values(hours, minutes, seconds):
+    with pytest.raises(ValueError):
+        Time(hours, minutes, seconds)
+
+
+
+@pytest.mark.parametrize("hours", list(range(0, 24)))
+def test_set_valid_hours(time, hours):
+    time.hours = hours
+    assert time.hours == hours
+
+
+@pytest.mark.parametrize("minutes", list(range(0, 60)))
+def test_set_valid_minutes(time, minutes):
+    time.minutes = minutes
+    assert time.minutes == minutes
+
+
+@pytest.mark.parametrize("seconds", list(range(0, 60)))
+def test_set_valid_seconds(time, seconds):
+    time.seconds = seconds
+    assert time.seconds == seconds
+
+
+@pytest.mark.parametrize("hours", [-4, -1, 24, 29])
+def test_set_invalid_hours(time, hours):
+    with pytest.raises(ValueError):
+        time.hours = hours
+
+@pytest.mark.parametrize("minutes", [-4, -1, 60, 61])
+def test_set_invalid_minutes(time, minutes):
+    with pytest.raises(ValueError):
+        time.minutes = minutes
+
+
+@pytest.mark.parametrize("seconds", [-4, -1, 60, 61])
+def test_set_invalid_seconds(time, seconds):
+    with pytest.raises(ValueError):
+        time.seconds = seconds
