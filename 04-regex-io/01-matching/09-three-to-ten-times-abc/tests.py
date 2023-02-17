ecd6cee8951d4f5050680e6f6cbceafc456e5a4b
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-17 11:33:46 +0100

    04-regex-io/01-matching/09-three-to-ten-times-abc

--- 04-regex-io/01-matching/09-three-to-ten-times-abc/tests.py
+++ 04-regex-io/01-matching/09-three-to-ten-times-abc/tests.py
@@ -16,7 +16,9 @@ import solution
 ])
 def test_function(string):
     function_name = 'three_to_ten_times_abc'
-    assert hasattr(student, function_name), f"Missing function {function_name}"
+    if not hasattr(student, function_name):
+        pytest.skip(f"Missing function {function_name}")
+
 
     solution_function = getattr(solution, function_name)
     student_function = getattr(student, function_name)
