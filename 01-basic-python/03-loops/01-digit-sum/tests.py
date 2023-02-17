12aa0bab860bce13e1fddf2221e2ab24ed7b40e0
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-17 11:34:35 +0100

    01-basic-python/03-loops/01-digit-sum

--- 01-basic-python/03-loops/01-digit-sum/tests.py
+++ 01-basic-python/03-loops/01-digit-sum/tests.py
@@ -6,7 +6,9 @@ import solution
 @pytest.mark.parametrize("n", [0, 1, 4, 9, 10, 19, 231, 849, 23892380, 380928390185])
 def test_last_digit(n):
     function_name = 'last_digit'
-    assert hasattr(student, function_name), f"Missing function {function_name}"
+    if not hasattr(student, function_name):
+        pytest.skip(f"Missing function {function_name}")
+
 
     solution_function = getattr(solution, function_name)
     student_function = getattr(student, function_name)
