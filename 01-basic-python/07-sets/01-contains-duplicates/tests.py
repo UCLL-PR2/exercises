dbcf4615efd1d3f8420e04ef9a2791135f6074c6
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-17 15:04:47 +0100

    01-basic-python/07-sets/01-contains-duplicates

--- 01-basic-python/07-sets/01-contains-duplicates/tests.py
+++ 01-basic-python/07-sets/01-contains-duplicates/tests.py
@@ -1,16 +1,27 @@
-from contextlib import contextmanager
-from scripting.testing import test
-from scripting.quick import reference_based_test
-from scripting.reference import active_reference_implementation_from_id, reference_file
+import pytest
+import student
+import solution
 
 
-with reference_file('solution.py'):
-    with reference_based_test('contains_duplicates') as testcase:
-        testcase([])
-        testcase([1])
-        testcase([1, 1])
-        testcase([1, 2, 1])
-        testcase([3, 1, 2, 1])
-        testcase([3, 6, 2, 1])
-        testcase([3, 6, 2, 1, 6])
-        testcase([3, 6, 2, 1, 5])
+@pytest.mark.parametrize("xs", [
+    [],
+    [1],
+    [1, 2],
+    [1, 1],
+    [1, 1, 2, 2, 3, 3],
+    [1, 2, 3, 2, 1],
+    [1, 2, 3, 4, 5, 6],
+])
+
+def test_function(xs):
+    function_name = 'contains_duplicates'
+    if not hasattr(student, function_name):
+        pytest.skip(f"Missing function {function_name}")
+
+    solution_function = getattr(solution, function_name)
+    student_function = getattr(student, function_name)
+
+    actual = student_function(xs)
+    expected = solution_function(xs)
+
+    assert expected == actual, f"Wrong result for {xs}, expected {expected}, received {actual}"
