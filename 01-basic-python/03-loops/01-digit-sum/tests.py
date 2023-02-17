3c787a3009b0a30295b0171be722df48f938c42b
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-17 10:42:32 +0100

    01-basic-python/03-loops/01-digit-sum

--- 01-basic-python/03-loops/01-digit-sum/tests.py
+++ 01-basic-python/03-loops/01-digit-sum/tests.py
@@ -1,10 +1,17 @@
-from contextlib import contextmanager
-from scripting.testing import test
-from scripting.quick import reference_based_test
-from scripting.reference import active_reference_implementation_from_id, reference_file
+import pytest
+import student
+import solution
 
 
-with reference_file('solution.py'):
-    with reference_based_test('digit_sum') as testcase:
-        for x in range(1, 1000):
-            testcase(x)
+@pytest.mark.parametrize("n", [0, 1, 4, 9, 10, 19, 231, 849, 23892380, 380928390185])
+def test_function(n):
+    function_name = 'digit_sum'
+    assert hasattr(student, function_name), f"Missing function {function_name}"
+
+    solution_function = getattr(solution, function_name)
+    student_function = getattr(student, function_name)
+
+    actual = student_function(n)
+    expected = solution_function(n)
+
+    assert expected == actual, f"Wrong result for {n}, expected {expected}, received {actual}"
