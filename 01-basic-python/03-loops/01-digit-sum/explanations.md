0ff4458d00d8f149a0ac629a29a3059d050060d1
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-17 10:55:09 +0100

    01-basic-python/03-loops/01-digit-sum

--- 01-basic-python/03-loops/01-digit-sum/explanations.md
+++ 01-basic-python/03-loops/01-digit-sum/explanations.md
@@ -1,21 +1,14 @@
-# Assignment - difficulty level: *
+# Assignment
 
 `digit_sum` computes the sum of all the decimal digits of a given number `n`.
 For example, `digit_sum(159)` returns `15` because 1+5+9=15.
 
-```javascript
-function digit_sum(n)
-{
-    let result = 0;
+Start off with a function `last_digit` that returns the last digit of a number.
+For example, `last_digit(491)` should return `1`.
+Hint: modulo.
 
-    while ( n > 0 )
-    {
-        result += n % 10;
-        n = Math.floor(n / 10);
-    }
+Next, write a function `remove_last_digit` that cuts off a number's last digit.
+`remove_last_digit(1481)` should return `148`.
+Hint: integer division.
 
-    return result;
-}
-```
-
-Translate this function into Python.
+Finally, use these two functions to implement `digits_sum`.
--- 01-basic-python/03-loops/01-digit-sum/tests.py
+++ 01-basic-python/03-loops/01-digit-sum/tests.py
@@ -4,7 +4,35 @@ import solution
 
 
 @pytest.mark.parametrize("n", [0, 1, 4, 9, 10, 19, 231, 849, 23892380, 380928390185])
-def test_function(n):
+def test_last_digit(n):
+    function_name = 'last_digit'
+    assert hasattr(student, function_name), f"Missing function {function_name}"
+
+    solution_function = getattr(solution, function_name)
+    student_function = getattr(student, function_name)
+
+    actual = student_function(n)
+    expected = solution_function(n)
+
+    assert expected == actual, f"Wrong result for {n}, expected {expected}, received {actual}"
+
+
+@pytest.mark.parametrize("n", [0, 1, 4, 9, 10, 19, 231, 849, 23892380, 380928390185])
+def test_remove_last_digit(n):
+    function_name = 'remove_last_digit'
+    assert hasattr(student, function_name), f"Missing function {function_name}"
+
+    solution_function = getattr(solution, function_name)
+    student_function = getattr(student, function_name)
+
+    actual = student_function(n)
+    expected = solution_function(n)
+
+    assert expected == actual, f"Wrong result for {n}, expected {expected}, received {actual}"
+
+
+@pytest.mark.parametrize("n", [0, 1, 4, 9, 10, 19, 231, 849, 23892380, 380928390185])
+def test_digit_sum(n):
     function_name = 'digit_sum'
     assert hasattr(student, function_name), f"Missing function {function_name}"
 
