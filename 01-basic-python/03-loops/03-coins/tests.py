0fc80316294287ff4a0fec06689069feb9780fb9
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-17 11:19:40 +0100

    01-basic-python/03-loops/03-coins

--- 01-basic-python/03-loops/03-coins/tests.py
+++ 01-basic-python/03-loops/03-coins/tests.py
@@ -1,23 +1,33 @@
-from contextlib import contextmanager
-from scripting.testing import test
-from scripting.quick import reference_based_test
-from scripting.reference import active_reference_implementation_from_id, reference_file
+import pytest
+import student
+import solution
 
 
-with reference_file('solution.py'):
-    with reference_based_test('coins') as testcase:
-        testcase(0, 0, 0, 0)
-        testcase(0, 0, 0, 1)
-        testcase(1, 0, 0, 1)
-        testcase(1, 0, 0, 2)
-        testcase(2, 0, 0, 2)
-        testcase(0, 1, 0, 2)
-        testcase(0, 1, 0, 5)
-        testcase(0, 0, 1, 5)
-        testcase(2, 0, 1, 5)
-        testcase(2, 0, 1, 6)
-        testcase(2, 0, 1, 7)
-        testcase(2, 0, 1, 8)
-        testcase(2, 1, 1, 8)
-        testcase(0, 0, 1, 4)
-        testcase(0, 1, 1, 6)
+@pytest.mark.parametrize("one, two, five, goal", [
+    (0, 0, 0, 0),
+    (0, 0, 0, 1),
+    (1, 0, 0, 1),
+    (1, 0, 0, 2),
+    (2, 0, 0, 2),
+    (0, 1, 0, 2),
+    (0, 1, 0, 5),
+    (0, 0, 1, 5),
+    (2, 0, 1, 5),
+    (2, 0, 1, 6),
+    (2, 0, 1, 7),
+    (2, 0, 1, 8),
+    (2, 1, 1, 8),
+    (0, 0, 1, 4),
+    (0, 1, 1, 6),
+])
+def test_function(one, two, five, goal):
+    function_name = 'coins'
+    assert hasattr(student, function_name), f"Missing function {function_name}"
+
+    solution_function = getattr(solution, function_name)
+    student_function = getattr(student, function_name)
+
+    actual = student_function(one=one, two=two, five=five, goal=goal)
+    expected = solution_function(one=one, two=two, five=five, goal=goal)
+
+    assert expected == actual, f"Wrong result for {(one, two, five, goal)}, expected {expected}, received {actual}"
