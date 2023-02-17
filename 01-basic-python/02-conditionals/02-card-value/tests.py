cca4a97ccc8e28472c05549ef77643cedd7304f6
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-17 11:34:29 +0100

    01-basic-python/02-conditionals/02-card-value

--- 01-basic-python/02-conditionals/02-card-value/tests.py
+++ 01-basic-python/02-conditionals/02-card-value/tests.py
@@ -6,7 +6,9 @@ import solution
 @pytest.mark.parametrize("string", [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Ace', 'Jack', 'Queen', 'King' ])
 def test_function(string):
     function_name = 'card_value'
-    assert hasattr(student, function_name), f"Missing function {function_name}"
+    if not hasattr(student, function_name):
+        pytest.skip(f"Missing function {function_name}")
+
 
     solution_function = getattr(solution, function_name)
     student_function = getattr(student, function_name)
