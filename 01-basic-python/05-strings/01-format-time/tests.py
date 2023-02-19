5ff2e06ba4eec191b51b5d7d170cb891fec8c366
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-19 16:00:58 +0100

    01-basic-python/05-strings/01-format-time

--- 01-basic-python/05-strings/01-format-time/tests.py
+++ 01-basic-python/05-strings/01-format-time/tests.py
@@ -3,12 +3,9 @@ import student
 import solution
 
 
-@pytest.mark.parametrize("hours, minutes, seconds", [
-    (h, m, s)
-    for h in [0, 1, 4, 10, 19, 23]
-    for m in [0, 9, 15, 59]
-    for s in [0, 9, 15, 59]
-])
+@pytest.mark.parametrize("hours", [0, 1, 4, 10, 19, 23])
+@pytest.mark.parametrize("minutes", [0, 9, 15, 59])
+@pytest.mark.parametrize("seconds", [0, 9, 15, 59])
 def test_function(hours, minutes, seconds):
     function_name = 'format_time'
     if not hasattr(student, function_name):
