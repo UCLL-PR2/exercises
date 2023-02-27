3c1c43c05ef53a9396431bc8654c5fb1eae854b9
Frederic Vogels <frederic.vogels@ucll.be>
2023-02-27 12:42:32 +0100

    todo/functional-programming/translate/group-movies-by-year

--- todo/functional-programming/translate/group-movies-by-year/solution.py
+++ todo/functional-programming/translate/group-movies-by-year/solution.py
@@ -1,6 +1,5 @@
 def group_movies_by_year(movies):
-    years = {movie.year for movie in movies}
     return {
         year: [movie.title for movie in movies if movie.year == year]
-        for year in years
+        for year in {movie.year for movie in movies}
     }
