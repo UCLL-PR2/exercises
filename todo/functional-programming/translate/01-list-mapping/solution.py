06608f6c3b26321e18a679d1da7f41bd797adbc1
Frederic Vogels <frederic.vogels@ucll.be>
2023-03-06 13:12:20 +0100

    todo/functional-programming/translate/01-list-mapping

--- todo/functional-programming/translate/01-list-mapping/solution.py
+++ todo/functional-programming/translate/01-list-mapping/solution.py
@@ -1,2 +1,14 @@
 def titles(movies):
     return [movie.title for movie in movies]
+
+
+def titles_and_years(movies):
+    return [(movie.title, movie.year) for movie in movies]
+
+
+def titles_and_actor_counts(movies):
+    return [(movie.title, len(movie.actors)) for movie in movies]
+
+
+def longest_runtime(movies):
+    return max([movie.runtime for movie in movies])
--- todo/functional-programming/translate/01-list-mapping/tests.py
+++ todo/functional-programming/translate/01-list-mapping/tests.py
@@ -7,17 +7,23 @@ import student
 import starter
 
 
-@pytest.mark.parametrize('module', [solution, student])
-def test_titles(module):
-    function_name = 'titles'
-
-    if not hasattr(module, function_name):
-        pytest.skip()
+@pytest.mark.parametrize('function_name', [
+    'titles',
+    'titles_and_years',
+    'titles_and_actor_counts',
+    'longest_runtime',
+])
+def test_titles(function_name):
+    if not hasattr(student, function_name):
+        pytest.skip(f"Missing function {function_name} in student module")
 
     imperative_function = getattr(starter, function_name)
-    functional_function = getattr(module, function_name)
+    functional_solution = getattr(solution, function_name)
+    functional_student = getattr(student, function_name)
 
     imperative_result = imperative_function(movies)
-    functional_result = functional_function(movies)
+    functional_solution_result = functional_solution(movies)
+    functional_student_result = functional_student(movies)
 
-    assert functional_result == imperative_result
+    assert functional_solution_result == imperative_result, "If this assertion fails, the solution is buggy. Should not happen"
+    assert functional_student_result == imperative_result
