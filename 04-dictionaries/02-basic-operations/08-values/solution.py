84f2abbd7ffdfb6b10c318d2d1c7953f6e6fd03d
Frederic Vogels <frederic.vogels@ucll.be>
2023-03-06 10:34:44 +0100

    04-dictionaries/02-basic-operations/08-values

--- 04-dictionaries/02-basic-operations/08-values/solution.py
+++ 04-dictionaries/02-basic-operations/08-values/solution.py
@@ -1,4 +1,2 @@
 def values(dictionary):
-    return dictionary.values()
-
-# In order to really conform to the assignment, we should explicitly convert it to a list: `list(dictionary.values())`.
+    return list(dictionary.values())
