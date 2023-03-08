0afc78aa002ffc9da126a7764a72c11bd0e571cf
Frederic Vogels <frederic.vogels@ucll.be>
2023-03-08 14:03:44 +0100

    removed exercise

--- todo/functional-programming/01-comprehensions/movies_with_actor/solution.py
+++
@@ -1,2 +0,0 @@
-def movies_with_actor(movies, actor):
-    return [movie.title for movie in movies if actor in movie.actors]
--- todo/functional-programming/01-comprehensions/movies_with_actor/starter.py
+++
@@ -1,6 +0,0 @@
-def movies_with_actor(movies, actor):
-    result = []
-    for movie in movies:
-        if actor in movie.actors:
-            result.append(movie.title)
-    return result
