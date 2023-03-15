15ce72770531770bf7e8d6359502c31f7aac7392
Frederic Vogels <frederic.vogels@ucll.be>
2023-03-15 13:29:13 +0100

    todo/functional-programming/09-generator-comprehension

--- todo/functional-programming/09-generator-comprehension/tests.py
+++ todo/functional-programming/09-generator-comprehension/tests.py
@@ -1,33 +1,26 @@
 import pytest
-try:
-    from student import is_prime
-except:
-    pass
-try:
-    from student import primes
-except:
-    pass
+import student
 
 
 def if_defined(function_name):
-    return pytest.mark.skipif(function_name not in globals(), reason=f'{function_name} has not been defined')
+    return pytest.mark.skipif(function_name not in dir(student), reason=f'{function_name} has not been defined')
 
 
 
 @if_defined('is_prime')
 @pytest.mark.parametrize('n', [2, 3, 5, 7, 11, 13, 97])
 def test_is_prime_on_primes(n):
-    assert is_prime(n)
+    assert student.is_prime(n)
 
 
 @if_defined('is_prime')
 @pytest.mark.parametrize('n', [0, 1, 4, 6, 8, 9, 10, 100])
 def test_is_prime_on_nonprimes(n):
-    assert not is_prime(n)
+    assert not student.is_prime(n)
 
 
 @if_defined('primes')
 def test_primes():
     expected = [2, 3, 5, 7, 11, 13, 17, 19, 23]
-    actual = primes()
+    actual = student.primes()
     assert all(x == y for x, y in zip(expected, actual))
