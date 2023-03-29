d5aad30d5e9616a1c14b64489bc4fbddc772206a
Frederic Vogels <frederic.vogels@ucll.be>
2023-03-29 16:38:16 +0200

    todo/functional-programming/12-linked-lists/01-creation

--- todo/functional-programming/12-linked-lists/01-creation/linkedlist.py
+++ todo/functional-programming/12-linked-lists/01-creation/linkedlist.py
@@ -1,10 +1,19 @@
+import itertools
+
+
 class Node:
     def __init__(self, value, next=None):
         self.value = value
         self.next = next
 
+    def __iter__(self):
+        current = self
+        while current:
+            yield current.value
+            current = current.next
+
     def __eq__(self, other):
         if isinstance(other, Node):
-            return (self.value, self.next) == (other.value, other.next)
+            return all(x == y for x, y in itertools.zip_longest(self, other, fillvalue=object()))
         else:
             return NotImplemented
